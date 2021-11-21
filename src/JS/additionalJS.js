5c3589aca1b005d7947d0fceaf611a0722aaffd1
maxdecken <max.decken40@gmail.com>
2021-11-21 19:11:46 +0100

    -small fixes

--- src/JS/additionalJS.js
+++ src/JS/additionalJS.js
@@ -1,3 +1,11 @@
 function versionInfo(){
-    alert("This is Version 1.0.")
+    alert("This is Version 1.0. Made by: Max Decken");
 }
+
+function disbaleDeveloperTools() {
+    if (devTools.checked == true){
+        document.getElementById("box").className = "visiblePanel";
+    }else{
+        document.getElementById("box").className = "invisiblePanel";
+    }
+  }
